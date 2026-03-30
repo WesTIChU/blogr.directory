e28d94021098157dfa7fc39b5bfe37570c9e0862
Paul K <paul@example.com>
2026-03-30 14:01:04 +0100

    Update site content and navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,44 +2,55 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'BlogR Directory',
 			customCss: ['./src/styles/custom.css'],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			components: {
+				SiteTitle: './src/components/Logo.astro',
+			},
+			social: [
+				{
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/withastro/starlight',
+				},
+			],
 			sidebar: [
-            {
-                label: 'Getting started',
-                items: [
-					{ label: '🧠 About', slug: 'about' },
-                ],
-            },
-            {
-                label: 'Blog Directory',
-                items: [
-					{ label: '🧠 Personal Writing', slug: 'blogs/personal-writing' },
-					{ label: '🌍 Culture & Places', slug: 'blogs/culture-and-places' },
-					{ label: '✈️ Travel', slug: 'blogs/travel' },
-					{ label: '💻 Tech & Development', slug: 'blogs/tech-blogs' },
-					{ label: '💰 Finance, Bitcoin & Crypto', slug: 'blogs/finance-bitcoin-crypto' },
-                ],
-            },
-                        {
-                label: 'Communities',
-                items: [
-                    { label: 'Forums', slug: 'communties/forums' },
-                    { label: 'Small Communities', slug: 'communties/small-communities' },
-                ],
-            },
-                        {
-                label: 'Website Tools & Hosting', 
-                items: [
-                    { label: '⚙️ Blogging Platforms', slug: 'website-tools/blogging-platforms' },
-					{ label: '🌐️ Hosting Providers', slug: 'website-tools/hosting-providers' },
-                ],
-            },
+				{
+					label: 'Getting started',
+					items: [
+						{ label: '🧠 About', slug: 'about' },
+						{ label: '🧠 Contribute', slug: 'contribute' },
+					],
+				},
+				{
+					label: 'Blog Directory',
+					items: [
+						{ label: '🧠 Personal Writing', slug: 'blogs/personal-writing' },
+						{ label: '🌍 Culture & Places', slug: 'blogs/culture-and-places' },
+						{ label: '✈️ Travel', slug: 'blogs/travel' },
+						{ label: '💻 Tech & Development', slug: 'blogs/tech-blogs' },
+						{ label: '💰 Finance, Bitcoin & Crypto', slug: 'blogs/finance-bitcoin-crypto' },
+					],
+				},
+				{
+					label: 'Communities',
+					items: [
+						{ label: 'Forums', slug: 'communties/forums' },
+						{ label: 'Small Communities', slug: 'communties/small-communities' },
+					],
+				},
+				{
+					label: 'Build Your Own Site',
+					items: [
+						{ label: '⚙️ Blogging Platforms', slug: 'website-tools/blogging-platforms' },
+						{ label: '🌐️ Hosting', slug: 'website-tools/hosting' },
+						{ label: '🌐️ Self-Hosting', slug: 'website-tools/self-hosting' },
+						{ label: '🌐️ Self-Hosting Apps', slug: 'website-tools/self-hosting-apps' },
+					],
+				},
 			],
 		}),
 	],
