282aa66c9b83111befbc6e31fe16465871f8697a
Paul K <paul@example.com>
2026-03-30 14:13:51 +0100

    Update site content and navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,13 +22,13 @@ export default defineConfig({
 					label: 'Getting started',
 					items: [
 						{ label: '🧠 About', slug: 'about' },
-						{ label: '🧠 Contribute', slug: 'contribute' },
+						{ label: '🤝 Contribute', slug: 'contribute' },
 					],
 				},
 				{
 					label: 'Blog Directory',
 					items: [
-						{ label: '🧠 Personal Writing', slug: 'blogs/personal-writing' },
+						{ label: '📝 Personal Writing', slug: 'blogs/personal-writing' },
 						{ label: '🌍 Culture & Places', slug: 'blogs/culture-and-places' },
 						{ label: '✈️ Travel', slug: 'blogs/travel' },
 						{ label: '💻 Tech & Development', slug: 'blogs/tech-blogs' },
@@ -38,17 +38,17 @@ export default defineConfig({
 				{
 					label: 'Communities',
 					items: [
-						{ label: 'Forums', slug: 'communties/forums' },
-						{ label: 'Small Communities', slug: 'communties/small-communities' },
+						{ label: '💬 Forums', slug: 'communities/forums' },
+						{ label: '🌱 Small Communities', slug: 'communities/small-communities' },
 					],
 				},
 				{
 					label: 'Build Your Own Site',
 					items: [
 						{ label: '⚙️ Blogging Platforms', slug: 'website-tools/blogging-platforms' },
-						{ label: '🌐️ Hosting', slug: 'website-tools/hosting' },
-						{ label: '🌐️ Self-Hosting', slug: 'website-tools/self-hosting' },
-						{ label: '🌐️ Self-Hosting Apps', slug: 'website-tools/self-hosting-apps' },
+						{ label: '🌐 Hosting', slug: 'website-tools/hosting' },
+						{ label: '🖥️ Self-Hosting', slug: 'website-tools/self-hosting' },
+						{ label: '🧩 Self-Hosting Apps', slug: 'website-tools/self-hosting-apps' },
 					],
 				},
 			],
