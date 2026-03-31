db1ef231d963c5b8304e9851f356cf3d2e424b9a
Paul K <paul@example.com>
2026-03-31 20:21:09 +0100

    removed emojis from nav bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,34 +28,34 @@ export default defineConfig({
         {
           label: 'Getting started',
           items: [
-            { label: '🧠 About', slug: 'about' },
-            { label: '🤝 Contribute', slug: 'contribute' },
+            { label: 'About', slug: 'about' },
+            { label: 'Contribute', slug: 'contribute' },
           ],
         },
         {
           label: 'Blog Directory',
           items: [
-            { label: '📝 Personal Writing', slug: 'blogs/personal-writing' },
-            { label: '🌍 Culture & Places', slug: 'blogs/culture-and-places' },
-            { label: '✈️ Travel', slug: 'blogs/travel' },
-            { label: '💻 Tech & Development', slug: 'blogs/tech-blogs' },
-            { label: '💰 Finance, Bitcoin & Crypto', slug: 'blogs/finance-bitcoin-crypto' },
+            { label: 'Personal Writing', slug: 'blogs/personal-writing' },
+            { label: 'Culture & Places', slug: 'blogs/culture-and-places' },
+            { label: 'Travel', slug: 'blogs/travel' },
+            { label: 'Tech & Development', slug: 'blogs/tech-blogs' },
+            { label: 'Finance, Bitcoin & Crypto', slug: 'blogs/finance-bitcoin-crypto' },
           ],
         },
         {
           label: 'Communities',
           items: [
-            { label: '💬 Forums', slug: 'communities/forums' },
-            { label: '🌱 Small Communities', slug: 'communities/small-communities' },
+            { label: 'Forums', slug: 'communities/forums' },
+            { label: 'Small Communities', slug: 'communities/small-communities' },
           ],
         },
         {
           label: 'Build Your Own Site',
           items: [
-            { label: '⚙️ Blogging Platforms', slug: 'website-tools/blogging-platforms' },
-            { label: '🌐 Hosting', slug: 'website-tools/hosting' },
-            { label: '🖥️ Self-Hosting', slug: 'website-tools/self-hosting' },
-            { label: '🧩 Self-Hosting Apps', slug: 'website-tools/self-hosting-apps' },
+            { label: 'Blogging Platforms', slug: 'website-tools/blogging-platforms' },
+            { label: 'Hosting', slug: 'website-tools/hosting' },
+            { label: 'Self-Hosting', slug: 'website-tools/self-hosting' },
+            { label: 'Self-Hosting Apps', slug: 'website-tools/self-hosting-apps' },
           ],
         },
       ],
